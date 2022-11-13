782db1776f45c0b176fffc53281a94cac62b61e8
Nuisancexd <76814800+Nuisancexd@users.noreply.github.com>
2022-11-13 15:02:35 +0500

    Add files via upload

---
+++ remove elements vector.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+
+int remove_elem(std::vector<int>& nums, int val)
+{
+	int k = 0;
+	for (int i = nums.size() - 1; i >= 0; i--)
+	{
+		if (nums[i] == val)
+		{
+			nums.erase(nums.begin() + i);
+		}
+		k++;
+	}
+	for (auto elements : nums)
+	{
+		std::cout << elements;
+	}
+	std::cout << std::endl;
+	return k;
+}
+
+int main()
+{
+	std::vector<int> vec = { 3, 2, 2, 3};
+	std::cout << remove_elem(vec, 3);
+	
+	return 0;
+}
