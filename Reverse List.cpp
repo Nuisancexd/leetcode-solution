29853f6ae18ca950759f3dec4adb05b5b5f7c74f
Nuisancexd <76814800+Nuisancexd@users.noreply.github.com>
2022-11-24 21:05:22 +0500

    Add files via upload

---
+++ Reverse List.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+struct ListNode 
+{
+    
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head)
+    {
+        ListNode* temp = NULL;
+        ListNode* prev = NULL;
+
+        while (head != NULL)
+        {
+            temp = head->next;
+            head->next = prev;
+            prev = head;
+            head = temp;
+        }
+
+        return prev;
+    }
+};
